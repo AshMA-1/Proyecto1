cc88debaa9611c66f95827c01d499451eafa2092
ashle <ashle@LAPTOP-K5QB7NRG>
2020-11-01 16:00:43 -0600

    Add 5

---
+++ src/proyecto1/Problema5.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ashle
+ */
+public class Problema5 {
+    
+static Scanner entrada = new Scanner(System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int decimal, digito;
+        
+        System.out.print("Favor ingreasr la cantidad");
+        int cantidad = entrada.nextInt();
+        
+     
+    }
+    
+}
