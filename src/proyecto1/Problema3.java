e7eccd76edd61d1ff31472215c5b6a7277c97be8
ashle <ashle@LAPTOP-K5QB7NRG>
2020-11-01 22:48:42 -0600

    Ajustes 3

--- src/proyecto1/Problema3.java
+++ src/proyecto1/Problema3.java
@@ -16,13 +16,16 @@ public class Problema3 {
     /**
      * @param the args command line arguments
      */
-    public void calcularImpuesto(int numDependientes, double salario){
+    public static void main(String[] args) {
         // TODO code application logic here
+        int numDependientes;
+        double salario;
         Scanner calcular = new Scanner(System.in);
         double arreglo[] = {0, 1, 2, 3, 4};
         double a = 0,b = 0,c = 0,d = 0,e = 0;
         int valor = 0;
         int ingreso;
+        int resulado;
         
         arreglo[0] = 1;
         arreglo[1] = 0.9;
@@ -30,7 +33,7 @@ public class Problema3 {
         arreglo[3] = 0.6;
         arreglo[4] = 0.4;
         
-        System.out.println("Presione el salario aproximado:\n 1) Salario menor a 200.000\n 2)Salario igual a 200.000 y menor a 400.000\n 3) Salario igual a 400.000 y menor a 600.000\n 4) Salario igual a 600.000 y menor a 800.000\n 5) Salario mayor o igual a 800.000\n");
+        System.out.println("Presione el salario aproximado:\n 1) Salario menor a 200.000\n 2) Salario entre 200.000 y menor a 400.000\n 3) Salario entre 400.000 y menor a 600.000\n 4) Salario entre 600.000 y menor a 800.000\n 5) Salario mayor o igual a 800.000\n");
         salario = calcular.nextInt();
         
         System.out.println("Ingrese aquí su salario: ");
@@ -62,15 +65,16 @@ public class Problema3 {
                 break;
 
             case 5:
-                e = salario * arreglo[4];
+                e = salario * 0.4;
                 //TRIANGULO
                 System.out.println("El Impuesto de Renta a pagar es: " + e);
                 break;
-        }
+                
 
+        }
+        
     }
-public static void main(String[] args) {
-}
+
     
 }
 
