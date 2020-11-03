455cc406b41b0ff7cbcea6f89d9942603f52eaaf
ashle <ashle@LAPTOP-K5QB7NRG>
2020-11-02 22:44:12 -0600

    add problema 2.2

---
+++ src/proyecto1/Problema22.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ashle
+ */
+public class Problema22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n,x;
+        
+        double elementos, numeros = 0;
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("¿Cuántos dígitos va a ingresar?: ");
+        n=entrada.nextInt();
+   
+        for(int i=1;i<=n;i++)
+        { 
+         System.out.println("Digite el número "+i);
+         x=entrada.nextInt();
+         numeros=numeros+x;                
+        }   
+        elementos=numeros/n;
+        
+         System.out.println("El Promedio es:"+elementos);                               
+    }   
+    }
+    
+
