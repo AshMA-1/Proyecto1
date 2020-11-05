25a1927d9aa1bc94036f3cc15803558025fd88b1
ashle <ashle@LAPTOP-K5QB7NRG>
2020-11-04 23:30:09 -0600

    ultimo detalle de problema 4

--- src/proyecto1/Problema4.java
+++ src/proyecto1/Problema4.java
@@ -18,17 +18,20 @@ public class Problema4 {
     /**
      * @param args the command line arguments
      */
-     public static boolean biciesto(int anno){
+    public static boolean biciesto(int anno) {
+        //Inicializar variables 
         int ano;
         int p = 4;
         int q = 100;
         int r = 400;
         boolean resultado = false;
 
+        //Permite que el usuario digite
         Scanner entrada = new Scanner(System.in);
         System.out.print("Ingrese año a evaluar: ");
         ano = entrada.nextInt();
 
+        //Inicia mi condición de si es o no año bisiesto, ciclo con IF 
         if ((ano % p == 0) && (ano % q != 0 || ano % r == 0)) {
             resultado = true;
             System.out.println(ano + " es un año bisiesto");
@@ -36,6 +39,8 @@ public class Problema4 {
             resultado = false;
             System.out.println(ano + " no es un año bisiesto");
         }
+
+        //Retorna el resultado 
         return resultado;
 
     }
