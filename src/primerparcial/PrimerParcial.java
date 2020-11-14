77f4ad0e7385e05db25e5ac6ef9adb55622bbdaf
ashle <ashle@LAPTOP-K5QB7NRG>
2020-11-14 16:31:08 -0600

    Add Primer Parcial

--- src/primerparcial/PrimerParcial.java
+++ src/primerparcial/PrimerParcial.java
@@ -5,6 +5,8 @@
  */
 package primerparcial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ashle
@@ -14,8 +16,62 @@ public class PrimerParcial {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
         // TODO code application logic here
+    public static void Inicializar(double edad[], String sobreNombre[], String apellidos[], String sexo[], String tipoSangre[], Scanner Entrada) {
+        // TODO code application logic here
+        for (int i = 0; i < edad.length; i++) {
+
+            System.out.print("Ingrese el nombre del paciente: ");
+            sobreNombre[i] = Entrada.next();
+            System.out.print("Ingrese el apellido del paciente: ");
+            apellidos[i] = Entrada.next();
+            System.out.print("Ingrese el gènero del paciente: ");
+            sexo[i] = Entrada.next();
+            System.out.print("Ingrese el tipo de Sangre del paciente: ");
+            tipoSangre[i] = Entrada.next();
+            System.out.print("Ingrese la edad: ");
+            edad[i] = Entrada.nextDouble();
+
+        }
     }
-    
+
+    public static double Riesgo(double edad[]) {
+        double num = 0.9, prom;
+        for (int i = 0; i < edad.length; i++) {
+
+        }
+
+        prom = num * edad.length;
+        return prom;
+    }
+
+    public static void main(String[] args) {
+        Scanner Entrada = new Scanner(System.in);
+
+        double edad[];
+        String sobreNombre[];
+        String apellidos[];
+        String sexo[];
+        String tipoSangre[];
+        int n;
+
+        System.out.print("Ingrese la cantidad de pacientes: ");
+        n = Entrada.nextInt();
+
+        edad = new double[n];
+        sobreNombre = new String[n];
+
+        edad = new double[n];
+        apellidos = new String[n];
+
+        edad = new double[n];
+        sexo = new String[n];
+
+        edad = new double[n];
+        tipoSangre = new String[n];
+
+        Inicializar(edad, sobreNombre, apellidos, sexo, tipoSangre, Entrada);
+        Riesgo(edad);
+
+    }
 }
