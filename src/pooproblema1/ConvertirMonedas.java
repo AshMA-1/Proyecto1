b5e115422f1f32b2dad847a835bd8dc7b5d34f65
ashle <ashle@LAPTOP-K5QB7NRG>
2020-12-13 23:21:20 -0600

    Proyecto2

---
+++ src/pooproblema1/ConvertirMonedas.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooproblema1;
+
+/**
+ *
+ * @author ashle
+ */
+public class ConvertirMonedas {
+
+    Moneda monedaColon;
+    Moneda monedaDolar;
+    Moneda monedaEuro;
+
+    public ConvertirMonedas() {
+    }
+
+    public ConvertirMonedas(Moneda monedaColon, Moneda monedaDolar, Moneda monedaEuro) {
+        this.monedaColon = monedaColon;
+        this.monedaDolar = monedaDolar;
+        this.monedaEuro = monedaEuro;
+    }
+
+    //Este metodo requiere que monedaOrigen y monedaSalida esten establecidas
+    public float convertir(Moneda monedaOrigen, Moneda monedaSalida, float cantidadOrigen) {
+        float resultado = 0;
+        if (monedaOrigen != null && monedaSalida != null) {
+
+            if (monedaOrigen.getNombre().equals("Colon")) {
+                if (monedaSalida.getNombre().equals("Colon")) {
+                    resultado = cantidadOrigen;
+                } else if (monedaSalida.getNombre().equals("Dolar")) {
+                    resultado = cantidadOrigen / monedaDolar.getValorColon();
+                } else {
+                    resultado = cantidadOrigen / monedaEuro.getValorColon();
+                }
+            } else if (monedaOrigen.getNombre().equals("Dolar")) {
+
+                if (monedaSalida.getNombre().equals("Colon")) {
+                    resultado = cantidadOrigen * monedaDolar.getValorColon();
+                } else if (monedaSalida.getNombre().equals("Dolar")) {
+                    resultado = cantidadOrigen;
+                } else {
+                    resultado = cantidadOrigen * monedaEuro.getValorColon();
+                }
+            } else {//origen euro
+                if (monedaSalida.getNombre().equals("Colon")) {
+                    resultado = cantidadOrigen * monedaEuro.getValorColon();
+                } else if (monedaSalida.getNombre().equals("Dolar")) {
+                    resultado = cantidadOrigen * (monedaEuro.getValorColon() / monedaDolar.getValorColon());
+                } else {
+                    resultado = cantidadOrigen;
+                }
+            }
+        }
+        return resultado;
+    }
+
+    public Moneda getMonedaColon() {
+        return monedaColon;
+    }
+
+    public void setMonedaColon(Moneda monedaColon) {
+        this.monedaColon = monedaColon;
+    }
+
+    public Moneda getMonedaDolar() {
+        return monedaDolar;
+    }
+
+    public void setMonedaDolar(Moneda monedaDolar) {
+        this.monedaDolar = monedaDolar;
+    }
+
+    public Moneda getMonedaEuro() {
+        return monedaEuro;
+    }
+
+    public void setMonedaEuro(Moneda monedaEuro) {
+        this.monedaEuro = monedaEuro;
+    }
+
+}
